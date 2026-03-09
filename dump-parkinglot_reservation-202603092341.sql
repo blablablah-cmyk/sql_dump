d77f1efedc5a5b7fb4b7bad5cd73132910bd6916
Tiramisu <ttrongkington@gmail.com>
2026-03-09 23:59:41 +0700

    Add files via upload

---
+++ dump-parkinglot_reservation-202603092341.sql
@@ -0,0 +1,198 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: parkinglot_reservation
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `booker`
+--
+
+DROP TABLE IF EXISTS `booker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `booker` (
+  `citizen_id` decimal(13,0) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `surname` varchar(30) NOT NULL,
+  `telephone` decimal(12,0) NOT NULL,
+  PRIMARY KEY (`citizen_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booker`
+--
+
+LOCK TABLES `booker` WRITE;
+/*!40000 ALTER TABLE `booker` DISABLE KEYS */;
+INSERT INTO `booker` VALUES (1579800010988,'sompong','jaiyai',898988854),(1579901070888,'somchai','jaidee',980088989),(1579901070889,'somying','jaided',955586432);
+/*!40000 ALTER TABLE `booker` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `car`
+--
+
+DROP TABLE IF EXISTS `car`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `car` (
+  `license_plate` varchar(50) NOT NULL,
+  `owner_id` decimal(13,0) NOT NULL,
+  `brand` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `model` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `color` varchar(30) NOT NULL,
+  `type` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`license_plate`),
+  KEY `car_relation_1` (`owner_id`),
+  CONSTRAINT `car_relation_1` FOREIGN KEY (`owner_id`) REFERENCES `booker` (`citizen_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `car`
+--
+
+LOCK TABLES `car` WRITE;
+/*!40000 ALTER TABLE `car` DISABLE KEYS */;
+INSERT INTO `car` VALUES ('กย4566 เชียงราย',1579800010988,'toyota','supra','ขาว','กระบะ'),('ทน1184 กระบี่',1579901070889,'ford','ranger','เเดง','กระบะ'),('บห7795 กรุงเทพมหานคร',1579901070888,'honda','civic','ดำ','เก๋ง');
+/*!40000 ALTER TABLE `car` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `parking`
+--
+
+DROP TABLE IF EXISTS `parking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `parking` (
+  `lot_id` varchar(5) NOT NULL,
+  `types` varchar(30) NOT NULL,
+  `CCTV` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`lot_id`),
+  KEY `parking_relation_1` (`types`),
+  CONSTRAINT `parking_relation_1` FOREIGN KEY (`types`) REFERENCES `types` (`lot_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `parking`
+--
+
+LOCK TABLES `parking` WRITE;
+/*!40000 ALTER TABLE `parking` DISABLE KEYS */;
+INSERT INTO `parking` VALUES ('A0001','norm','c0001'),('A0002','vip','c0002'),('B0001','disable','c0003');
+/*!40000 ALTER TABLE `parking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payment` (
+  `transaction_id` varchar(10) NOT NULL,
+  `reservation_id` varchar(10) NOT NULL,
+  `status` tinyint NOT NULL DEFAULT '0',
+  `total` decimal(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`transaction_id`),
+  KEY `payment_relation_1` (`reservation_id`),
+  CONSTRAINT `payment_relation_1` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`reservation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment`
+--
+
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+INSERT INTO `payment` VALUES ('t000000001','re00000001',0,200.00),('t000000002','re00000002',1,500.00),('t000000003','re00000003',1,75.00);
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservation` (
+  `reservation_id` varchar(10) NOT NULL,
+  `license_plate` varchar(50) NOT NULL,
+  `reserved_lot` varchar(5) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `duration_hour` decimal(6,1) NOT NULL,
+  PRIMARY KEY (`reservation_id`),
+  KEY `reservation_relation_1` (`license_plate`),
+  KEY `reservation_relation_2` (`reserved_lot`),
+  CONSTRAINT `reservation_relation_1` FOREIGN KEY (`license_plate`) REFERENCES `car` (`license_plate`),
+  CONSTRAINT `reservation_relation_2` FOREIGN KEY (`reserved_lot`) REFERENCES `parking` (`lot_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservation`
+--
+
+LOCK TABLES `reservation` WRITE;
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
+INSERT INTO `reservation` VALUES ('re00000001','กย4566 เชียงราย','A0001','2025-02-01','12:00:00',4.0),('re00000002','ทน1184 กระบี่','A0002','2025-02-02','11:30:00',2.5),('re00000003','บห7795 กรุงเทพมหานคร','B0001','2025-03-04','08:00:00',3.0);
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `types`
+--
+
+DROP TABLE IF EXISTS `types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `types` (
+  `lot_type` varchar(30) NOT NULL DEFAULT 'norm',
+  `rate_per_hr` decimal(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`lot_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `types`
+--
+
+LOCK TABLES `types` WRITE;
+/*!40000 ALTER TABLE `types` DISABLE KEYS */;
+INSERT INTO `types` VALUES ('disable',25.00),('norm',50.00),('vip',200.00);
+/*!40000 ALTER TABLE `types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'parkinglot_reservation'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-09 23:41:32
